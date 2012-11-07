26d6447d4edff93e325d5428d8e50112cd5f3096
nickolay <nickolay@bryntum.com>
2012-11-07 16:34:18 +0400

    - test

--- build/restart-server.sh
+++ build/restart-server.sh
@@ -8,6 +8,11 @@ cd $DIR/..
 git fetch --all
 git reset --hard origin/master
 
+echo "Stopping Fiesta server"
+
 screen -S fiesta-server -X kill
+
+echo "Starting Fiesta server"
+
 screen -d -m -S fiesta-server node app.js
 
