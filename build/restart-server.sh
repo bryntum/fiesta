8b6e3a42c0dadd67b3dd434ad29c0f8424352c75
nickolay <nickolay@bryntum.com>
2012-11-07 15:54:17 +0400

    - fixes

--- build/restart-server.sh
+++ build/restart-server.sh
@@ -3,11 +3,11 @@
 # get the directory in which the script reside
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-cd ..
+cd $DIR/..
 
 git fetch --all
 git reset --hard origin/master
 
-`screen -S fiesta-server -X kill`;
-`screen -d -m -S fiesta-server node app.js`;
+screen -S fiesta-server -X kill
+screen -d -m -S fiesta-server node app.js
 
