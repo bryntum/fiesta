ee8eba5214f8e4b35d3e9645343d0bd6e0207e52
mats <mats@mankz.com>
2013-05-02 08:45:44 +0200

    welcome page

--- media/js/fiesta/view/HomePanel.js
+++ media/js/fiesta/view/HomePanel.js
@@ -5,18 +5,18 @@ Ext.define('Fiesta.view.HomePanel', {
     initComponent : function () {
 
         Ext.apply(this, {
-            html : '<h2 style="color:#666;font-size:3em;margin:4% 0;text-align: center">Welcome to <b style="color:darkblue">Fiesta</b>, a crowd sourced test tool</h2>' +
-                '<div style="text-align: center;clear:both"><img src="/media/img/siestalogo.png" height="40%"/></div>'+
-                '<div style="position: absolute;bottom:4%;left:0;width:100%">' +
-                '<dl style="float:left;width:33%;border-right:1px solid #ddd;text-align: center;font-size:2em;color:#999">' +
-                    '<dt style="color:navy">1234</dt><dd>test cases</dd>' +
-                '</dl>' +
-                '<dl style="float:left;width:33%;border-right:1px solid #ddd;text-align: center;font-size:2em;color:#999">' +
-                    '<dt style="color:darkred">25</dt><dd>users online</dd>' +
-                '</dl>' +
-                '<dl style="float:left;width:33%;text-align: center;font-size:2em;color:#999">' +
-                    '<dt style="color:green">3253</dt><dd>othercool stat</dd>' +
-                '</dl>' +
+            html : '<h2 style="height:20%color:#666;font-size:3em;margin:4% 0;text-align: center">Welcome to <b style="color:darkblue">Fiesta</b>, a crowd sourced test tool</h2>' +
+                '<div style="height:50%;overflow:hidden;text-align: center;clear:both"><img style="height:20%" src="/media/img/siestalogo.png"/></div>'+
+                '<div style="background:#ededed;height:30%;width:100%">' +
+                    '<dl style="float:left;width:33%;border-right:1px solid #ddd;text-align: center;font-size:2em;color:#999">' +
+                        '<dt style="color:navy">1234</dt><dd>test cases</dd>' +
+                    '</dl>' +
+                    '<dl style="float:left;width:33%;border-right:1px solid #ddd;text-align: center;font-size:2em;color:#999">' +
+                        '<dt style="color:darkred">25</dt><dd>users online</dd>' +
+                    '</dl>' +
+                    '<dl style="float:left;width:33%;text-align: center;font-size:2em;color:#999">' +
+                        '<dt style="color:green">3253</dt><dd>othercool stat</dd>' +
+                    '</dl>' +
                 '</div>'
         });
 
