bb2660d1f964a810fb0cdf5cda964f6b198a8d2d
mats <mats@mankz.com>
2013-05-09 16:05:29 +0200

    CSS styling

--- media/js/fiesta/view/HomePanel.js
+++ media/js/fiesta/view/HomePanel.js
@@ -7,7 +7,6 @@ Ext.define('Fiesta.view.HomePanel', {
 
         Ext.apply(this, {
             html : '<h2>Welcome to <b style="color:darkblue">Fiesta</b>, a crowd sourced test tool</h2>' +
-                '<div class="logo-ct"><img style="height:20%" src="/media/img/siestalogo.png"/></div>'+
                 '<div class="footer">' +
                     '<dl>' +
                         '<dt style="color:navy">'+DATA.totalTests+'</dt><dd>test cases</dd>' +
