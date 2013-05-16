7d3d1878eaaa99a62a40068c78fbbe7faa333535
nickolay <nickolay@bryntum.com>
2013-05-16 18:18:18 +0400

    - test tweak

--- tests/unit/tags_field_focus.t.js
+++ tests/unit/tags_field_focus.t.js
@@ -25,6 +25,28 @@ StartTest(function(t) {
             })
         })
         
+        var field2      = new Fiesta.plugins.TagSelect({
+            renderTo            : Ext.getBody(),
+            
+            width               : 200,
+            
+            displayField        : "tag",
+            valueField          : "id",
+            emptyText           : "Filter by tag(s)",
+            queryMode           : 'local',
+            forceSelection      : true,
+            
+            store           : new Ext.data.Store({
+                fields      : [ 'tag' ],
+                
+                data        : [
+                    { id : 1, tag : "Grid" },
+                    { id : 2, tag : "GridPanel" },
+                    { id : 3, tag : "GridBox" }
+                ]
+            })
+        })
+        
         t.chain(
             { type : 'Grid,', target : field },
             function (next) {
@@ -43,6 +65,30 @@ StartTest(function(t) {
             { type : '[BACKSPACE]', target : field },
             function (next) {
                 t.is(document.activeElement, field.el.down('.x-boxselect-input-field', true))
+                
+                next()
+            },
+            
+            // to hide the dropdown of 1st field
+            { click : document.body },
+            
+            // now checking field is not cleared after typing tag + comma
+            { type : 'Grid,', target : field2 },
+            function (next) {
+                t.ok(field2.getRawValue(), "Field is not empty")
+                
+                t.isDeeply(field2.getValue(), [ 1 ], "Correct array with id's array")
+                
+                next()
+            },
+            
+            { type : '[BACKSPACE]grid,', target : field2 },
+            function (next) {
+                t.ok(field2.getRawValue(), "Field is not empty")
+                
+                t.isDeeply(field2.getValue(), [ 1 ], "Tags selection should be case-insensitive")
+                
+                next()
             }
         )        
     })
