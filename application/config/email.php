3bd0aed689107cad2f1dceb309ed34eb2790d7f9
d4rk <d4rkalx@gmail.com>
2013-06-07 12:24:33 +0400

    email config

---
+++ application/config/email.php
@@ -0,0 +1,14 @@
+<?php
+$config['useragent'] = 'FiestaMailer';
+$config['protocol'] = 'sendmail';
+$config['mailpath'] = '/usr/sbin/sendmail';
+$config['smtp_host'] = '';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['smtp_port'] = '25';
+$config['smtp_timeout'] = '5';
+$config['wordwrap'] = TRUE;
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+
+?>
