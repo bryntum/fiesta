128ed4ea410ea8f14a9a97d728a3136f8d86f9d1
d4rk <d4rkalx@gmail.com>
2013-06-21 11:16:06 +0400

    second captcha in reset password dialog removed

--- application/controllers/account/reset_password.php
+++ application/controllers/account/reset_password.php
@@ -30,28 +30,32 @@ class Reset_password extends CI_Controller {
 		// Redirect signed in users to homepage
 		if ($this->authentication->is_signed_in()) redirect('');
 
-		// Check recaptcha
-		$recaptcha_result = $this->recaptcha->check();
+
+// Uncoment this part to enable captcha before reset password dialogue
+        
+//		// Check recaptcha
+//		$recaptcha_result = $this->recaptcha->check();
 
 		// User has not passed recaptcha
-		if ($recaptcha_result !== TRUE)
-		{
-			if ($this->input->post('recaptcha_challenge_field'))
-			{
-				$data['reset_password_recaptcha_error'] = $recaptcha_result ? lang('reset_password_recaptcha_incorrect') : lang('reset_password_recaptcha_required');
-			}
-
-			// Load recaptcha code
-			$data['recaptcha'] = $this->recaptcha->load($recaptcha_result, $this->config->item("ssl_enabled"));
-
-			// Load reset password captcha view
-			$this->load->view('account/reset_password_captcha', isset($data) ? $data : NULL);
-			return;
-		}
+//		if ($recaptcha_result !== TRUE)
+//		{
+//			if ($this->input->post('recaptcha_challenge_field'))
+//			{
+//				$data['reset_password_recaptcha_error'] = $recaptcha_result ? lang('reset_password_recaptcha_incorrect') : lang('reset_password_recaptcha_required');
+//			}
+//
+//			// Load recaptcha code
+//			$data['recaptcha'] = $this->recaptcha->load($recaptcha_result, $this->config->item("ssl_enabled"));
+//
+//			// Load reset password captcha view
+//			$this->load->view('account/reset_password_captcha', isset($data) ? $data : NULL);
+//			return;
+//		}
 
 		// Get account by email
 		if ($account = $this->account_model->get_by_id($this->input->get('id')))
 		{
+
 			// Check if reset password has expired
 			if (now() < (strtotime($account->resetsenton) + $this->config->item("password_reset_expiration")))
 			{
