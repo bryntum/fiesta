8ee40452fa3203fa313ecef7f541ae6f3bccc792
Nickolay Platonov <nickolay@outpost.(none)>
2013-07-11 11:16:35 +0400

    - gzip backups

--- build/backup.sh
+++ build/backup.sh
@@ -8,7 +8,7 @@ DATE=$(date +%Y-%m-%d)
 AGO=$(date +%Y-%m-%d -d "14 days ago")
 AGO2=$(date +%Y-%m-%d -d "15 days ago")
 
-mysqldump --user=$FIESTA_MYSQL_USER --password=$FIESTA_MYSQL_PASSWORD fiesta > $FIESTA/backup/$DATE.sql
+mysqldump --user=$FIESTA_MYSQL_USER --password=$FIESTA_MYSQL_PASSWORD fiesta | gzip > $FIESTA/backup/$DATE.sql.gz
 
-rm $FIESTA/backup/$AGO.sql
-rm $FIESTA/backup/$AGO2.sql
+rm $FIESTA/backup/$AGO.sql.gz
+rm $FIESTA/backup/$AGO2.sql.gz
